9b161898ab8d5b65527925d9a4565f2491708f17
leberton <leberton@leberton42.com>
2025-07-10 21:29:04 +0200

    Base 2 for nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -35,3 +35,16 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 require("config.lazy")
 require("config.keymaps")
+
+vim.api.nvim_create_autocmd('LspAttach', {
+	desc = 'LSP actions',
+	callback = function(event)
+		local opts = {buffer = event.buf}
+		vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+		vim.keymap.set('n', '<leader>gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+		vim.keymap.set('n', '<leader>gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+		vim.keymap.set('n', '<leader>gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+		vim.keymap.set('n', '<leader>gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+		vim.keymap.set('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+	end
+})
--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -13,7 +13,7 @@ return {
 {
 	"neovim/nvim-lspconfig",
 	config = function()
-		require'lspconfig'.lua_ls.setup{}
+		require('lspconfig').lua_ls.setup{}
 		require('lspconfig').clangd.setup({
 		  cmd = { "/usr/bin/clangd" },
 		})
