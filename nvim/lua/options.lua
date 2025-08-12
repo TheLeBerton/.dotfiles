b7e25a596c714dfad01feec7a6df6ab859a6ca76
leberton_asahi <leberton@asahi.leo>
2025-08-12 14:02:03 +0200

    Disable this horrible mouse

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -4,3 +4,8 @@ vim.opt.termguicolors = true
 vim.opt.tabstop = 4
 vim.cmd("set clipboard=unnamedplus")
 vim.cmd("set noexpandtab")
+vim.keymap.set("", "<up>", "<nop>", { noremap = true })
+vim.keymap.set("", "<down>", "<nop>", { noremap = true })
+vim.keymap.set("i", "<up>", "<nop>", { noremap = true })
+vim.keymap.set("i", "<down>", "<nop>", { noremap = true })
+vim.opt.mouse = ""
