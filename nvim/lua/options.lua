a0287511f2d25cfd73f455720a4222771d2e19ba
leberton_asahi <leberton@asahi.leo>
2025-08-18 16:57:36 +0200

    Add colorcolumn

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -9,3 +9,4 @@ vim.keymap.set("", "<down>", "<nop>", { noremap = true })
 vim.keymap.set("i", "<up>", "<nop>", { noremap = true })
 vim.keymap.set("i", "<down>", "<nop>", { noremap = true })
 vim.opt.mouse = ""
+vim.cmd("set colorcolumn=80")
