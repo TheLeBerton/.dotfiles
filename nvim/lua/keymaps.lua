9a90d62875c521edd2eb06795938d9880f9562e1
leberton_asahi <leberton@asahi.leo>
2025-08-20 02:39:38 +0200

    Create set_keymap function

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,6 +1,10 @@
 vim.g.mapleader = ' '
 
-vim.keymap.set('n', '<leader>n', '<cmd>bn<cr>', { desc = "[B]uffer [N]ext" })
-vim.keymap.set('n', '<leader>p', '<cmd>bp<cr>', { desc = "[B]uffer [P]revious" })
-vim.keymap.set('n', '<leader>x', '<cmd>bd<cr>', { desc = "[B]uffer [D]elete" })
-vim.keymap.set('i', 'jk', '<Esc>', { desc = "Go to normal mode" })
+local function set_keymap(mode, keys, command, opts)
+  vim.keymap.set(mode, keys, command, opts)
+end
+
+set_keymap('n', '<leader>n', '<cmd>bn<cr>', { desc = "[B]uffer [N]ext" })
+set_keymap('n', '<leader>p', '<cmd>bp<cr>', { desc = "[B]uffer [P]revious" })
+set_keymap('n', '<leader>x', '<cmd>bd<cr>', { desc = "[B]uffer [D]elete" })
+set_keymap('i', 'jk', '<Esc>', { desc = "Go to normal mode" })
