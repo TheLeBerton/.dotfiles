2f0b7aa364902829469c70f4aab006af6cd5c68a
leberton_asahi <leberton@asahi.leo>
2025-09-09 12:34:09 +0200

    Nvim: configurate zenmode

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -18,10 +18,10 @@ return {
 						vim.keymap.set(mode, l, r, opts)
 					end
 
-					map('n', '<leader>gb', function() gs.blame_line{full=true} end, {desc = "Git blame line"})
-					map('n', '<leader>gp', gs.preview_hunk, {desc = "Preview hunk"})
-					map('n', '<leader>gr', gs.reset_hunk, {desc = "Reset hunk"})
-					map('n', '<leader>gs', gs.stage_hunk, {desc = "Stage hunk"})
+					map('n', '<leader>gb', function() gs.blame_line { full = true } end, { desc = "Git blame line" })
+					map('n', '<leader>gp', gs.preview_hunk, { desc = "Preview hunk" })
+					map('n', '<leader>gr', gs.reset_hunk, { desc = "Reset hunk" })
+					map('n', '<leader>gs', gs.stage_hunk, { desc = "Stage hunk" })
 				end
 			})
 		end
@@ -30,7 +30,7 @@ return {
 		"tpope/vim-fugitive",
 		cmd = { "Git", "G" },
 		keys = {
-			{ "<leader>gg", "<cmd>Git<cr>", desc = "Git status" },
+			{ "<leader>gg", "<cmd>Git<cr>",        desc = "Git status" },
 			{ "<leader>gc", "<cmd>Git commit<cr>", desc = "Git commit" },
 			{ "<leader>gd", "<cmd>Gdiffsplit<cr>", desc = "Git diff" },
 		}
