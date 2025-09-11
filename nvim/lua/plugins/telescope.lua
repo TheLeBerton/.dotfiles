9e0b99f64d9551e41bc75a896d45bf75c98808a2
leberton_asahi <leberton@asahi.leo>
2025-09-11 14:26:50 +0200

    Nvim: add telescope for buffer keybind

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -20,5 +20,6 @@ return {
 	vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope [F]ind [F]iles" })
 	vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope [F]ind [G]rep" })
 	vim.keymap.set("n", "<leader>fw", builtin.grep_string, { desc = "Telescope [F]ind [W]ord" })
+	vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope [F]ind [B]uffer" })
     end
 }
