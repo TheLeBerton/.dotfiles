da5c83350adafbdfd40bcb562c0b0eebcf6df7ea
leberton_asahi <leberton@asahi.leo>
2025-10-19 13:21:52 +0200

    Add autocmds file to nvim

---
+++ nvim/lua/config/autocmds.lua
@@ -0,0 +1,16 @@
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 40,
+		})
+	end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	callback = function()
+		local save_cursor = vim.fn.getpos(".")
+		vim.cmd([[%s/\s\+$//e]])
+		vim.fn.setpos(".", save_cursor)
+	end,
+})
