2dfcf896ec9706940afc1ecbdf102b43d77ba742
Leonardo Bertonasco <leberton@c2r6p1.42vienna.com>
2025-11-29 11:15:04 +0100

    Fix dvorak

--- shell/zsh/modules/07-keyboard.zsh
+++ shell/zsh/modules/07-keyboard.zsh
@@ -1,5 +1,13 @@
 #!/usr/bin/env zsh
 
-if [[ $IS_42 -eq 1 ]]; then
-	setxkbmap -I$HOME/.xkb -layout real-prog-dvorak
+if [[ "$IS_42" -eq 1 ]]; then
+    xkbcomp -I"$HOME/.xkb" - "$DISPLAY" <<EOF
+xkb_keymap {
+    xkb_keycodes { include "evdev+aliases(qwerty)" };
+    xkb_types    { include "complete" };
+    xkb_compat   { include "complete" };
+    xkb_symbols  { include "pc+realpdv+inet(evdev)+capslock(ctrl_modifier)" };
+    xkb_geometry { include "pc(pc105)" };
+};
+EOF
 fi
