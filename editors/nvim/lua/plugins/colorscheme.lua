991ab1c9669e288dd2d2dfa42c87819d9307643f
TheLeBerton <theleberton@gmail.com>
2025-12-19 00:21:55 +0100

    Change to oxocarbon theme

--- editors/nvim/lua/plugins/colorscheme.lua
+++ editors/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-	"folke/tokyonight.nvim",
+	"nyoom-engineering/oxocarbon.nvim",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd.colorscheme("tokyonight-night")
+		vim.cmd.colorscheme("oxocarbon")
 	end,
 }
