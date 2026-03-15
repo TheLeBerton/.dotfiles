a99e6d39a7df34526f5146c52815da597ed14907
leberton <theleberton@gmail.com>
2026-03-16 00:23:24 +0100

    feat(nvim): add gitblame inline

--- editors/nvim/lua/plugins/gitsigns.lua
+++ editors/nvim/lua/plugins/gitsigns.lua
@@ -1,4 +1,11 @@
 return {
 	"lewis6991/gitsigns.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" }
+	dependencies = { "nvim-lua/plenary.nvim" },
+	opts = {
+		current_line_blame = true,
+		current_line_blame_opts = {
+			delay = 500,
+			virt_text_pos = "eol",
+		},
+	},
 }
