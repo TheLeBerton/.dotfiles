e4d164194ab6f0c9048d46af7971ea1e93c8b883
leberton <theleberton@gmail.com>
2026-03-19 21:31:03 +0100

    feat(hypr): better overall theme

---
+++ desktop/rofi/theme.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+options="Gruvbox\nNord\nTokyo Night\nCatppuccin\nDracula"
+chosen=$(echo -e "$options" | rofi -dmenu -p "Theme" -theme ~/.config/rofi/pywal.rasi)
+WALLPAPER_DIR="$HOME/Pictures/wallpapers"
+
+case $chosen in
+	"Gruvbox")
+		wal --theme base16-gruvbox-hard
+		swww img "$WALLPAPER_DIR/gruvbox.png" --transition-type wipe --transition-duration 1
+		;;
+	"Nord")
+		wal --theme base16-nord
+		swww img "$WALLPAPER_DIR/nord.jpg" --transition-type wipe --transition-duration 1
+		;;
+	"Tokyo Night")
+		wal --theme tokyonight-moon
+		swww img "$WALLPAPER_DIR/tokyo-night.jpg" --transition-type wipe --transition-duration 1
+		;;
+	"Catppuccin")
+		wal --theme catppuccin-mocha
+		swww img "$WALLPAPER_DIR/catppuccin.jpg" --transition-type wipe --transition-duration 1
+		;;
+	"Dracula")
+		wal --theme base16-dracula
+		swww img "$WALLPAPER_DIR/dracula.jpg" --transition-type wipe --transition-duration 1
+		;;
+esac
+
+pywalfox update
+$HOME/.config/hypr/apply-colors.sh
