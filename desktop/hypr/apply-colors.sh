940fa512a3848c00f752823d6940a4fd7719d79a
leberton <theleberton@gmail.com>
2026-03-20 10:00:41 +0100

    feat(swaync): add swaync config

--- desktop/hypr/apply-colors.sh
+++ desktop/hypr/apply-colors.sh
@@ -4,15 +4,12 @@ source $HOME/.cache/wal/colors.sh
 
 active="${color4#\#}"
 inactive="${color0#\#}"
-$red = "${color1#\#}"
-$green = "${color2#\#}"
-$yellow = "${color3#\#}"
-$purple = "${color5#\#}"
-$aqua = "${color6#\#}"
-$fg = "${foreground#\#}"
 
 hyprctl keyword general:col.active_border "rgba(${active}ee)"
 hyprctl keyword general:col.inactive_border "rgba(${inactive}44)"
 
 pkill waybar
 waybar -c ~/.config/waybar/config.json &
+
+pkill mako
+swaync-client --reload-css &
