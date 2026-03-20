3013499a4044b1c3c7a4567cc55212e32d4b6b7c
leberton <theleberton@gmail.com>
2026-03-20 09:45:53 +0100

    feat(hypr): split looks into separate file

--- desktop/hypr/apply-colors.sh
+++ desktop/hypr/apply-colors.sh
@@ -4,6 +4,12 @@ source $HOME/.cache/wal/colors.sh
 
 active="${color4#\#}"
 inactive="${color0#\#}"
+$red = "${color1#\#}"
+$green = "${color2#\#}"
+$yellow = "${color3#\#}"
+$purple = "${color5#\#}"
+$aqua = "${color6#\#}"
+$fg = "${foreground#\#}"
 
 hyprctl keyword general:col.active_border "rgba(${active}ee)"
 hyprctl keyword general:col.inactive_border "rgba(${inactive}44)"
