26be696761aec6dcf81eb809cc410bf7c29ee291
leberton <theleberton@gmail.com>
2026-03-24 11:08:43 +0100

    feat(rofi): update scripts to use new themes

--- desktop/rofi/scripts-menu.sh
+++ desktop/rofi/scripts-menu.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-THEME="~/.config/rofi/pywal.rasi"
+THEME="~/.config/rofi/squared.rasi"
 DOTFILES="$HOME/.dotfiles"
 
 declare -A scripts=(
 		["Toggle Waybar"]="$DOTFILES/desktop/waybar/scripts/launch_me.sh"
 		["Switcheroo"]="$DOTFILES/desktop/rofi/switcheroo.sh"
+		["Wallpaperoo"]="$DOTFILES/desktop/rofi/wallpaperoo.sh"
 )
 
 chosen=$(printf '%s\n' "${!scripts[@]}" | rofi -dmenu -p "Scripts" -theme "$THEME")
