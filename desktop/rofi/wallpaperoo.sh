48d02f63466ad569898cef60bb8e13ac9305db2c
leberton <theleberton@gmail.com>
2026-03-24 11:10:41 +0100

    feat(rofi): update wallpaper switcher

--- desktop/rofi/wallpaperoo.sh
+++ desktop/rofi/wallpaperoo.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 DOTFILES="$HOME/.dotfiles"
+THEME="$HOME/.config/rofi/wallpaperoo.rasi"
 WALLPAPER_DIR="$DOTFILES/desktop/wallpapers"
 
 
 entries=""
+count=0
 for f in $DOTFILES/desktop/wallpapers/*; do
-	name=$(basename "$f" | sed 's/\.[^.]*$//') # basename without extension
+	name=$(basename "$f" | sed 's/\.[^.]*$//')
 	entries+="$name\0icon\x1f$HOME/.cache/wallpaper-thumbs/$(basename $f)\n"
+	((count++))
 done
 
-chosen=$(echo -e "$entries" | rofi -dmenu -p "Switch Wallpaper" -show-icons \
-	-theme ~/.config/rofi/wallpaper-switcher.rasi) || exit 0
+chosen=$(echo -e "$entries" | rofi -dmenu -p "" -show-icons \
+	-mesg "$count wallpapers  ·  ← →  to navigate" \
+	-theme "$THEME") || exit 0
 
 chosen_file=$(basename "$(ls "$WALLPAPER_DIR/$chosen".*)")
 swww img "$WALLPAPER_DIR/$chosen_file" --transition-type wipe --transition-duration 1
