a1959ed60e4cfff37fca413c86b8021e5748acea
leberton <theleberton@gmail.com>
2026-03-24 11:08:43 +0100

    feat(rofi): update scripts to use new themes

--- desktop/rofi/menu.sh
+++ desktop/rofi/menu.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-THEME="~/.config/rofi/pywal.rasi"
+THEME="~/.config/rofi/squared.rasi"
 
 options=" Apps\n Theme\n Scripts\n Power"
 chosen=$(echo -e "$options" | rofi -dmenu -p "Menu" -theme "$THEME")
 case $chosen in
 	" Apps")
-		rofi -show drun -theme "$THEME"
+		rofi -show drun -show-icons -theme "$THEME"
 		;;
 	" Theme")
 		~/.config/rofi/theme.sh
