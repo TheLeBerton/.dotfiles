60a5fbf94ffccddb64d2a7fa09fd0e4f8d1a917e
leberton <theleberton@gmail.com>
2026-03-24 11:09:52 +0100

    feat(rofi): add light/dark choice to theme script

--- desktop/rofi/theme.sh
+++ desktop/rofi/theme.sh
@@ -1,40 +1,39 @@
 #!/bin/bash
 
-
-options="Gruvbox\nNord\nTokyo Night\nCatppuccin\nDracula\nCatppuccin Latte\nRose Pine Dawn"
-chosen=$(echo -e "$options" | rofi -dmenu -p "Theme" -theme ~/.config/rofi/pywal.rasi)
 WALLPAPER_DIR="$HOME/.dotfiles/desktop/wallpapers"
+THEME="$HOME/.config/rofi/squared.rasi"
+DARK_THEMES=("Gruvbox" "Nord" "Tokyo Night" "Catppuccin Mocha" "Dracula")
+LIGHT_THEMES=("Catppuccin Latte" "Rose Pine Dawn")
 
-case $chosen in
-	"Gruvbox")
-		wal --theme base16-gruvbox-hard
-		swww img "$WALLPAPER_DIR/gruvbox.png" --transition-type wipe --transition-duration 1
-		;;
-	"Nord")
-		wal --theme base16-nord
-		swww img "$WALLPAPER_DIR/nord.jpg" --transition-type wipe --transition-duration 1
-		;;
-	"Tokyo Night")
-		wal --theme tokyonight-moon
-		swww img "$WALLPAPER_DIR/tokyo-night.jpg" --transition-type wipe --transition-duration 1
-		;;
-	"Catppuccin")
-		wal --theme catppuccin-mocha
-		swww img "$WALLPAPER_DIR/catppuccin.jpg" --transition-type wipe --transition-duration 1
-		;;
-	"Dracula")
-		wal --theme base16-dracula
-		swww img "$WALLPAPER_DIR/dracula.jpg" --transition-type wipe --transition-duration 1
-		;;
-	"Catppuccin Latte")
-		wal --theme catppuccin-latte -l
-		swww img "$WALLPAPER_DIR/catppuccin_latte.jpg" --transition-type wipe --transition-duration 1
-		;;
-	"Rose Pine Dawn")
-		wal --theme rose-pine-dawn -l
-		swww img "$WALLPAPER_DIR/catppuccin_latte.jpg" --transition-type wipe --transition-duration 1
-		;;
-esac
+declare -A THEME_DATA=(
+	["Gruvbox"]="base16-gruvbox-hard gruvbox.png"
+	["Nord"]="base16-nord nord.jpg"
+	["Tokyo Night"]="tokyonight-moon tokyo-night.jpg"
+	["Catppuccin Mocha"]="catppuccin-mocha catppuccin.jpg"
+	["Dracula"]="base16-dracula dracula.jpg"
+	["Catppuccin Latte"]="catppuccin-latte catppuccin_latte.jpg"
+	["Rose Pine Dawn"]="rose-pine-dawn catppuccin_latte.jpg"
+)
 
-pywalfox update
-$HOME/.config/hypr/apply-colors.sh
+pick_da_theeme() {
+	local mode
+	mode=$(printf 'Dark\nLight' | rofi -dmenu -p "Mode" -theme "$THEME")
+	[ -z "$mode" ] && return
+
+	local theme flags=""
+	if [[ "$mode" == "Light" ]]; then
+		theme=$(printf '%s\n' "${LIGHT_THEMES[@]}" | rofi -dmenu -p "Choose Theme" -theme "$THEME")
+		flags="-l"
+	else
+		theme=$(printf '%s\n' "${DARK_THEMES[@]}" | rofi -dmenu -p "Choose Theme" -theme "$THEME")
+	fi
+	[ -z "$theme" ] && return
+
+	read -r theme_name wallpaper <<< "${THEME_DATA[$theme]}"
+	wal --theme "$theme_name" $flags
+	swww img "$WALLPAPER_DIR/$wallpaper" --transition-type wipe --transition-duration 1
+	pywalfox update
+	$HOME/.config/hypr/apply-colors.sh
+}
+
+pick_da_theeme
