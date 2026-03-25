a46bd1540213737a6fa6e632b027b1ff7f2ae50a
theleberton <theleberton@gmail.com>
2026-03-25 10:02:00 +0100

    fix(nvim): update treesitter

--- editors/nvim/lua/plugins/treesitter.lua
+++ editors/nvim/lua/plugins/treesitter.lua
@@ -12,13 +12,5 @@ return {
 			"markdown",
 			"json",
 		},
-		auto_install = true,
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = false,
-		},
-		indent = {
-			enable = true,
-		},
 	},
 }
