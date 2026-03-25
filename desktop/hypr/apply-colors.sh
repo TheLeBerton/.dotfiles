d9df1003888d5852148c76bf211817353b19eac9
theleberton <theleberton@gmail.com>
2026-03-25 09:34:48 +0100

    feat(themes): add squared waybar css and update pick_da_theeme

--- desktop/hypr/apply-colors.sh
+++ desktop/hypr/apply-colors.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+WAYBAR_CONF=$HOME/.config/waybar
+
 source $HOME/.cache/wal/colors.sh
+
+waybar_style=$1
 
 active="${color4#\#}"
 inactive="${color0#\#}"
@@ -9,7 +13,7 @@ hyprctl keyword general:col.active_border "rgba(${active}ee)"
 hyprctl keyword general:col.inactive_border "rgba(${inactive}44)"
 
 pkill waybar
-waybar -c ~/.config/waybar/config.json &
+waybar -c $WAYBAR_CONF/config.json -s $WAYBAR_CONF/$waybar_style
 
 pkill mako
 swaync-client --reload-css &
