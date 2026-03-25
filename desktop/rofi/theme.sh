6eedd720d3f99d75890e5efaad403b54023ca0f2
theleberton <theleberton@gmail.com>
2026-03-25 09:34:48 +0100

    feat(themes): add squared waybar css and update pick_da_theeme

--- desktop/rofi/theme.sh
+++ desktop/rofi/theme.sh
@@ -6,14 +6,14 @@ DARK_THEMES=("Gruvbox" "Nord" "Tokyo Night" "Catppuccin Mocha" "Dracula" "Gray")
 LIGHT_THEMES=("Catppuccin Latte" "Rose Pine Dawn")
 
 declare -A THEME_DATA=(
-	["Gruvbox"]="base16-gruvbox-hard gruvbox.png 0"
-	["Nord"]="base16-nord nord.jpg 0"
-	["Tokyo Night"]="tokyonight-moon tokyo-night.jpg 10"
-	["Catppuccin Mocha"]="catppuccin-mocha catppuccin.jpg 10"
-	["Dracula"]="base16-dracula dracula.jpg 0"
-	["Gray"]="gray gray.jpg 0"
-	["Catppuccin Latte"]="catppuccin-latte catppuccin_latte.jpg 10"
-	["Rose Pine Dawn"]="rose-pine-dawn catppuccin_latte.jpg 0"
+	["Gruvbox"]="base16-gruvbox-hard gruvbox.png 0 squared.css"
+	["Nord"]="base16-nord nord.jpg 0 squared.css"
+	["Tokyo Night"]="tokyonight-moon tokyo-night.jpg 10 style.css"
+	["Catppuccin Mocha"]="catppuccin-mocha catppuccin.jpg 10 style.css"
+	["Dracula"]="base16-dracula dracula.jpg 0 squared.css"
+	["Gray"]="gray gray.jpg 0 squared.css"
+	["Catppuccin Latte"]="catppuccin-latte catppuccin_latte.jpg 10 style.css"
+	["Rose Pine Dawn"]="rose-pine-dawn catppuccin_latte.jpg 0 squared.css"
 )
 
 
@@ -31,11 +31,11 @@ pick_da_theeme() {
 	fi
 	[ -z "$theme" ] && return
 
-	read -r theme_name wallpaper rounding <<< "${THEME_DATA[$theme]}"
+	read -r theme_name wallpaper rounding waybar_style <<< "${THEME_DATA[$theme]}"
 	wal --theme "$theme_name" $flags
 	swww img "$WALLPAPER_DIR/$wallpaper" --transition-type wipe --transition-duration 1
 	pywalfox update
-	$HOME/.config/hypr/apply-colors.sh
+	$HOME/.config/hypr/apply-colors.sh "$waybar_style"
 	hyprctl keyword decoration:rounding "$rounding"
 }
 
