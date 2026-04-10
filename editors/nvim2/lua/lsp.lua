02d4114dc7fcab0be5660b2b49eb5016b92b938b
leberton <theleberton@gmail.com>
2026-04-11 00:50:24 +0200

    refactor( nvim2 ): use blink.cmp instead of default completion

--- editors/nvim2/lua/lsp.lua
+++ editors/nvim2/lua/lsp.lua
@@ -24,22 +24,9 @@ local start_servers = function()
 end
 
 
-local setup_completion = function()
-	vim.api.nvim_create_autocmd("LspAttach", {
-		callback = function(args)
-			local client = vim.lsp.get_client_by_id(args.data.client_id)
-			if client and client:supports_method("textDocument/completion") then
-				vim.lsp.completion.enable(true, args.data.client_id, args.buf, { autotrigger = true } )
-			end
-		end
-	})
-end
-
-
 M.setup = function()
 	init_mason()
 	start_servers()
-	setup_completion()
 end
 
 
