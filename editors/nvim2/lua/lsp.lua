d453719681149769b32e1b55fad90713d77c127d
leberton <theleberton@gmail.com>
2026-04-13 21:36:38 +0200

    refactor( nvim2 ): add more config to lua_ls

--- editors/nvim2/lua/lsp.lua
+++ editors/nvim2/lua/lsp.lua
@@ -2,7 +2,9 @@ local M = {}
 
 
 M.servers = {
-	["lua_ls"] = { cmd = { "lua-language-server" }, filetypes = { "lua" }, settings = { Lua = { diagnostics = { globals = { "vim" } } } } },
+	["lua_ls"] = { cmd = { "lua-language-server" }, filetypes = { "lua" }, settings = { Lua = {
+		diagnostics = { globals = { "vim" } }, workspace = { library = vim.api.nvim_get_runtime_file( "", true ) }
+	} } },
 	["pyright"] = { cmd = { "pyright-langserver", "--stdio" }, filetypes = { "python" }, root_markers = { "pyrightconfig.json", ".git" }, settings = { python = { analysis = { autoSearchPaths = true, useLibraryCodeForTypes = true, diagnosticMode = "workspace" } } } },
 	["clangd"] = { cmd = { "clangd" }, filetypes = { "c", "cpp" } }
 }
@@ -23,10 +25,18 @@ local start_servers = function()
 	end
 end
 
+local function init_loved2d()
+	vim.pack.add({
+		{ src = "https://github.com/S1M0N38/love2d.nvim" }
+	})
+	require( "love2d" ).setup()
+end
+
 
 M.setup = function()
 	init_mason()
 	start_servers()
+	init_loved2d()
 end
 
 
